de75daa6b653dc1f69c417c527755f618bf05c60
Abdelali Duiche <abdodui70@gmail.com>
2025-06-19 16:40:14 +0100

    Add stagiaire profile screen.

--- lib/pages/stagiaire/screens/profile_stagiaire.dart
+++ lib/pages/stagiaire/screens/profile_stagiaire.dart
@@ -1,3 +1,4 @@
+import 'package:cmc_travel_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class ProfileStagiaire extends StatefulWidget {
@@ -10,15 +11,121 @@ class ProfileStagiaire extends StatefulWidget {
 class _ProfileStagiaireState extends State<ProfileStagiaire> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Profile',
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          color: Color.fromARGB(255, 26, 142, 234),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0.0,
+        title: Text(
+          'Edit Profile',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: kDefaultPadding / 2),
+            child: TextButton(
+              onPressed: () => {},
+              child: Text(
+                'Done',
+                style: TextStyle(
+                  color: kPrimaryColor,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(kDefaultPadding),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  // backgroundImage: AssetImage('assets/images/profile_pic.jpg'),
+                  child: Icon(Icons.person, size: 60),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Username',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            TextButton(
+              onPressed: () => {},
+              child: Text(
+                'Change Profile Picture',
+                style: TextStyle(
+                  color: kPrimaryColor,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            _infoLabel('Full Name'),
+            _infosField(TextInputType.text, null),
+            SizedBox(height: 15),
+            _infoLabel('Phone Number'),
+            _infosField(
+              TextInputType.number,
+              Text('+212 | ', style: TextStyle(color: Colors.grey)),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Row _infosField(TextInputType? keyBoard, Widget? prefix) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(top: 10),
+            alignment: Alignment.center,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.black12.withAlpha(10),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: TextField(
+              keyboardType: keyBoard,
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                filled: false,
+                focusedBorder: InputBorder.none,
+                border: OutlineInputBorder(borderSide: BorderSide.none),
+                contentPadding: EdgeInsets.only(left: 10, right: 20),
+                constraints: BoxConstraints(maxHeight: 35),
+                prefix: prefix,
+                suffixIcon: Icon(
+                  Icons.check_rounded,
+                  size: 20,
+                  color: kPrimaryColor,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row _infoLabel(String content) {
+    return Row(
+      children: [
+        Text(
+          content,
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+        ),
+      ],
     );
   }
 }
