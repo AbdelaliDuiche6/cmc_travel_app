b5dee0b2d44593619904be9e2302a55c551e0d13
Abdelali Duiche <abdodui70@gmail.com>
2025-06-19 16:01:40 +0100

    Add a constructor method to fetch travel infos and create Travel instances.

--- lib/models/travel.dart
+++ lib/models/travel.dart
@@ -2,7 +2,16 @@ class Travel {
   final String name;
   final String location;
   final String imagePath;
-  final double rating;
+  final double price;
 
-  Travel(this.name, this.location, this.imagePath, this.rating);
+  Travel(this.name, this.location, this.imagePath, this.price);
+
+  factory Travel.fromMap(Map<String, dynamic> map) {
+    return Travel(
+      map['title'],
+      map['type'],
+      map['image_url'],
+      (map['price_per_person'] as num).toDouble(),
+    );
+  }
 }
