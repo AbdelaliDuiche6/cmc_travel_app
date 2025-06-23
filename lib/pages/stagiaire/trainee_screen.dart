abcab0ac53aaa27a21e6d5515efb66f65159d6d5
Abdelali Duiche <abdodui70@gmail.com>
2025-06-23 17:46:58 +0100

    Remove supabase initialization for debuging purpose.

--- lib/pages/stagiaire/trainee_screen.dart
+++ lib/pages/stagiaire/trainee_screen.dart
@@ -6,17 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:cmc_travel_app/constants.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-void main() async {
-  await Supabase.initialize(
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoZHR6eGRyYXV3YWltb25iamt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTAzNzUsImV4cCI6MjA2MzMyNjM3NX0.ht0Yn2byTGRjyRVJa7kFBVCwT3hk-PCuKf_jcVnQEvw",
-    url: "https://fhdtzxdrauwaimonbjkz.supabase.co",
-  );
-
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TraineeScreen()));
-}
 
 class TraineeScreen extends StatefulWidget {
   const TraineeScreen({super.key});
