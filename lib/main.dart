0551b2ffe6360337b5d53db64ce5bc5da51cbb2a
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-06-23 17:38:22 +0100

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
-
 import 'package:cmc_travel_app/pages/splash_screen.dart';
+
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async{
+
+void main() async {
+
+  // Initialisation de Supabase
   await Supabase.initialize(
-    anonKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoZHR6eGRyYXV3YWltb25iamt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTAzNzUsImV4cCI6MjA2MzMyNjM3NX0.ht0Yn2byTGRjyRVJa7kFBVCwT3hk-PCuKf_jcVnQEvw",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoZHR6eGRyYXV3YWltb25iamt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTAzNzUsImV4cCI6MjA2MzMyNjM3NX0.ht0Yn2byTGRjyRVJa7kFBVCwT3hk-PCuKf_jcVnQEvw",
     url: "https://fhdtzxdrauwaimonbjkz.supabase.co",
   );
 
-runApp(const MyApp());
-  }
+  // final notificationService = NotificationService();
+  // await notificationService.initFCM();
+  //
+  // // Définir la fonction pour gérer les messages en arrière-plan
+  // FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
 
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
 
