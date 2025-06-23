e055531518015304dd293f4f10379dd5feab78a1
Abdelali Duiche <abdodui70@gmail.com>
2025-06-23 17:50:31 +0100

    Add fetched data for travel details + payment method setting.

--- lib/pages/stagiaire/screens/travel_details.dart
+++ lib/pages/stagiaire/screens/travel_details.dart
@@ -1,31 +1,248 @@
 import 'package:flutter/material.dart';
+import 'package:another_flushbar/flushbar.dart';
+
+import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:cmc_travel_app/constants.dart';
 
+import '../../../models/travel.dart';
 import '../components/back_button.dart';
-import '../widgets/payment.dart';
 
 class TravelDetails extends StatefulWidget {
-  const TravelDetails({super.key});
+  const TravelDetails({super.key, required this.travel});
 
+  final Travel travel;
   @override
   State<TravelDetails> createState() => _TravelDetailsState();
 }
 
 class _TravelDetailsState extends State<TravelDetails> {
+  final supabase = Supabase.instance.client;
+  String organizerName = '';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  void _showFlushbar(String message) {
+    Flushbar(
+      message: message,
+      duration: Duration(seconds: 3),
+      backgroundColor: Colors.black87,
+      margin: EdgeInsets.all(16),
+      borderRadius: BorderRadius.circular(12),
+      flushbarPosition: FlushbarPosition.TOP,
+      animationDuration: Duration(milliseconds: 500),
+      icon: Icon(Icons.info_outline, color: Colors.white),
+    ).show(context);
+  }
+
+  Future<void> _insertReservation(bool paymentState) async {
+    final userId = supabase.auth.currentUser?.id;
+    if (userId == null) return;
+
+    final existing = await supabase
+        .from('Reservation')
+        .select()
+        .eq('voyage_id', widget.travel.id)
+        .eq('stagiaire_id', userId);
+
+    if (existing.isNotEmpty) {
+      Navigator.pop(context);
+      _showFlushbar('You already booked this trip!');
+      return;
+    }
+    try {
+
+     await supabase.from('Reservation').insert({
+      'stagiaire_id': userId,
+      'voyage_id': widget.travel.id,
+      'payment_state': paymentState,
+      'organizer_id': widget.travel.organizerId,
+     });
+
+     if(paymentState) {
+        // Stripe only
+        await supabase.from('Voyage')
+          .update({'free_places': widget.travel.freePlaces - 1})
+          .eq('id', widget.travel.id);
+
+        setState(() {
+          widget.travel.freePlaces--;
+        });
+      }
+
+     if (!mounted) return;
+
+      Navigator.pop(context);
+      _showFlushbar('Reservation created successfully!');
+    }catch(e) {
+      if (!mounted) return;
+      _showFlushbar('Error creating reservation: ${e.toString()}');
+    }
+    
+  }
+
   void _openPaymentOverlay() {
+    int? selectedPayment;
     showModalBottomSheet(
       context: context,
       isDismissible: true,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
-      builder: (ctx) => Payment(),
+      builder:
+          (ctx) => StatefulBuilder(
+            builder:
+                (context, setModalState) => Material(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            'Payment  Method',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.3,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.grey.withAlpha(30),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(
+                                  left: kDefaultPadding,
+                                  right: kDefaultPadding,
+                                ),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/images/stripe.png',
+                                      width: 30,
+                                      height: 30,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      'Stripe',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Radio(
+                                      value: 1,
+                                      groupValue: selectedPayment,
+                                      onChanged: (value) {
+                                        setModalState(
+                                          () => selectedPayment = value as int,
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                  left: kDefaultPadding,
+                                  right: kDefaultPadding,
+                                ),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.monetization_on_rounded,
+                                      size: 30,
+                                      color: Colors.green,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      'Cash',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Radio(
+                                      value: 2,
+                                      groupValue: selectedPayment,
+                                      onChanged: (value) {
+                                        setModalState(
+                                          () => selectedPayment = value as int,
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            if (selectedPayment == null) {
+                              _showFlushbar('Please select a payment method!');
+                              return;
+                            }
+                            _insertReservation(selectedPayment == 1);
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: kPrimaryColor,
+                            foregroundColor: Colors.white,
+                            padding: EdgeInsets.symmetric(
+                              horizontal: kDefaultPadding * 1.5,
+                              vertical: kDefaultPadding / 2,
+                            ),
+                          ),
+                          child: Text(
+                            'Pay',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+          ),
     );
   }
 
+  
   @override
   Widget build(BuildContext context) {
+    final String formattedDate = DateFormat(
+      'd MMM yyyy',
+    ).format(widget.travel.date);
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -47,7 +264,12 @@ class _TravelDetailsState extends State<TravelDetails> {
                           ),
                         ],
                       ),
-                      child: Image.asset('assets/images/travel.png'),
+                      child: Image.network(
+                        widget.travel.imagePath,
+                        fit: BoxFit.cover,
+                        errorBuilder:
+                            (context, error, stackTrace) => Icon(Icons.image),
+                      ),
                     ),
                   ),
                   CustomBackButton(
@@ -62,10 +284,13 @@ class _TravelDetailsState extends State<TravelDetails> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    _travelInfo('via', 'CMC'),
-                    _travelInfo('type', 'Fishing'),
-                    _travelInfo('rating', '4.5'),
-                    _travelInfo('seats', '40'),
+                    _travelInfo('via', widget.travel.organizerName),
+                    _travelInfo('type', widget.travel.type),
+                    _travelInfo(
+                      'free seats',
+                      widget.travel.freePlaces.toString(),
+                    ),
+                    _travelInfo('seats', widget.travel.places.toString()),
                   ],
                 ),
               ),
@@ -80,7 +305,6 @@ class _TravelDetailsState extends State<TravelDetails> {
                 child: Text(
                   'Description',
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  // textAlign: TextAlign.start,
                 ),
               ),
               Padding(
@@ -89,9 +313,7 @@ class _TravelDetailsState extends State<TravelDetails> {
                   left: kDefaultPadding,
                   right: kDefaultPadding,
                 ),
-                child: Text(
-                  'this is a description of the travelsdkvsfgnbotinb dp fdgrmdpievm nfeirgpgmmbmtoiunbvmkdfvlmeituyhjldmbnoognbitnkbmbbntoiprevmd;fmdbitrbniroreppbm botir', // textAlign: TextAlign.start,
-                ),
+                child: Text(widget.travel.description),
               ),
               Row(
                 children: [
@@ -106,7 +328,6 @@ class _TravelDetailsState extends State<TravelDetails> {
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                       ),
-                      // textAlign: TextAlign.start,
                     ),
                   ),
                   Container(
@@ -124,7 +345,7 @@ class _TravelDetailsState extends State<TravelDetails> {
                       borderRadius: BorderRadius.circular(12),
                     ),
                     child: Text(
-                      '15 Dec - 20 Dec 2025',
+                      formattedDate,
                       style: TextStyle(color: Colors.black.withAlpha(150)),
                     ),
                   ),
@@ -138,7 +359,7 @@ class _TravelDetailsState extends State<TravelDetails> {
                     TextSpan(
                       children: [
                         TextSpan(
-                          text: r'$59/',
+                          text: '\$${widget.travel.price}/',
                           style: TextStyle(
                             fontSize: 17,
                             fontWeight: FontWeight.bold,
@@ -163,7 +384,7 @@ class _TravelDetailsState extends State<TravelDetails> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextButton(
-                    onPressed: _openPaymentOverlay,
+                    onPressed: widget.travel.freePlaces == 0 ? null : _openPaymentOverlay,
                     style: TextButton.styleFrom(
                       backgroundColor: kPrimaryColor,
                       foregroundColor: Colors.white,
