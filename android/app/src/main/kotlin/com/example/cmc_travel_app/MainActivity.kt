9ce916b12305f31ca42d7bfb1903de76122b09f0
Abdelali Duiche <abdodui70@gmail.com>
2025-06-25 12:25:19 +0100

    Edit android / kotlin config files for Stripe setup.

--- android/app/src/main/kotlin/com/example/cmc_travel_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cmc_travel_app/MainActivity.kt
@@ -2,4 +2,7 @@ package com.example.cmc_travel_app
 
 import io.flutter.embedding.android.FlutterActivity
 
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+
 class MainActivity : FlutterActivity()
