d0739234bf8f7c552fffe949c510e79897f3b0cf
Abdelali Duiche <abdodui70@gmail.com>
2025-06-25 12:28:12 +0100

    Add stripe config files to setup the payment method.

---
+++ lib/config/constants.dart
@@ -0,0 +1,2 @@
+const String supabaseRestUrl = 'https://fhdtzxdrauwaimonbjkz.supabase.co/functions/v1/super-handler';
+const String supabaseFunctionUrl = 'https://fhdtzxdrauwaimonbjkz.supabase.co';
---
+++ lib/config/stripe_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+class StripeConfig {
+  static void init() {
+    Stripe.publishableKey = 'pk_test_51RdeASH8W1zP12YHZ2CCCN70B9xCkair4FEgfaCQJdLlkzAT7HbKHuAqUK5WJrjN7Ei5wMy5rYMdLB4VXQoH3Y4600E5K0SlIp'; 
+  }
+}
